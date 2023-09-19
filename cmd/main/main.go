b101b716f27e587c7c6b3ec4f556eece34157b29
ichelyadinov <ichelyadinov@ozon.ru>
2023-09-19 20:37:53 +0200

    added logger

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"projects/go-auth/config"
+	"projects/go-auth/internal/logger"
+)
+
+func main() {
+	// Init config.
+	cfg := config.InitConfig()
+
+	// Get logger.
+	logger := logger.NewLogger(cfg)
+
+}
